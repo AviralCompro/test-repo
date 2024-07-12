1eb3903ef98a9ad5f29509d8fd9b28eafb65e719
Aviral Juyal <aviral.juyal@comprotechnologies.com>
2024-07-12 15:35:39 +0530

    intial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,50 +1,31 @@
-import { resolve } from 'path';
-import { defineNuxtConfig } from 'nuxt/config';
+import {resolve} from "path"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  build: {
-    transpile: ['@heroicons/vue'],
-    extractCSS: true,
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          // Define a custom cache group to bundle all composables and utils
-          common: {
-            test: /[\\/]composables[\\/]|[\\/]utils[\\/]/,
-            name: 'common',
-            chunks: 'all',
-            enforce: true,
-          },
-        },
-      },
-    },
-    analyze: {
-      analyzerMode: 'static',
-      openAnalyzer: true, // Automatically open the analyzer in the browser
-    },
+  modules: ["@nuxtjs/tailwindcss"],
+  build:{
+    transpile:['@heroicons/vue']
   },
-  alias: {
-    '@': resolve(__dirname, '/'),
+  alias:{
+    "@":resolve(__dirname,"/")
   },
-  app: {
-    head: {
-      title: 'Twitter',
-    },
+  app:{
+    head:{
+      title:"Twitter"
+    }
   },
-  ssr: true,
-  runtimeConfig: {
+  ssr:true,
+  runtimeConfig:{
     jwtAccessSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
     cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
     cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
     cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+
   },
   server: {
     host: '0.0.0.0', // default: localhost
     port: 3000,      // default: 3000
   },
-});
+
+})
